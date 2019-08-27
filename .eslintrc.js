a63d67dbf98368f918fd1de0d93f8f843af825c1
mygit <mygit@163.com>
2019-08-27 14:50:19 +0800

    fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,54 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
+    commonjs: true
   },
   extends: 'eslint:recommended',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      experimentalObjectRestSpread: true,
+      jsx: true,
+      arrowFunctions: true,
+      classes: true,
+      modules: true,
+      defaultParams: true
     },
-    ecmaVersion: 2018,
     sourceType: 'module'
   },
+  parser: 'babel-eslint',
   plugins: ['react'],
-  rules: {}
+  rules: {
+    //"semi": ["error", "always"],
+    'no-empty': 0,
+    'comma-dangle': 0,
+    'no-unused-vars': 0,
+    'no-console': 0,
+    'no-const-assign': 2,
+    'no-dupe-class-members': 2,
+    'no-duplicate-case': 2,
+    'no-extra-parens': [2, 'functions'],
+    'no-self-compare': 2,
+    'accessor-pairs': 2,
+    'comma-spacing': [
+      2,
+      {
+        before: false,
+        after: true
+      }
+    ],
+    'constructor-super': 2,
+    'new-cap': [
+      2,
+      {
+        newIsCap: true,
+        capIsNew: false
+      }
+    ],
+    'new-parens': 2,
+    'no-array-constructor': 2,
+    'no-class-assign': 2,
+    'no-cond-assign': 2,
+    'no-mixed-spaces-and-tabs': 0
+  }
 };
