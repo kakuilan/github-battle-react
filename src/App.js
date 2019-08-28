ccecf0d677c40849096b0ada8133c05d9828dc10
mygit <mygit@163.com>
2019-08-28 18:40:37 +0800

    添加底部标签栏

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Drawer, List, NavBar, Icon, } from 'antd-mobile';
+import { Drawer, List, NavBar, TabBar, Icon, } from 'antd-mobile';
 import './App.less';
 import thumbImg from './assets/image/gray.png';
 
 class App extends React.Component {
   state = {
     open: true,
+    selectedTab: 'redTab',
+    hidden: false,
+    fullScreen: false,
   }
+
   onOpenChange = (...args) => {
     console.log(args);
     this.setState({ open: !this.state.open, });
   }
+
   render() {
     const menus = [
       {
@@ -57,10 +62,113 @@ class App extends React.Component {
         onOpenChange={this.onOpenChange}
         open={this.state.open}
         sidebar={sidebar}
-        style={{ minHeight: document.documentElement.clientHeight, }}
+        style={{ minHeight: document.documentElement.clientHeight - 110, }}
       >
         Github battle
       </Drawer>
+
+      <TabBar
+        barTintColor="white"
+        hidden={this.state.hidden}
+        tintColor="#33A3F4"
+        unselectedTintColor="#949494"
+      >
+        <TabBar.Item
+          badge={1}
+          data-seed="logId"
+          icon={<div style={{
+            width: '22px',
+            height: '22px',
+            background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat', }}
+                />
+          }
+          key="Life"
+          onPress={() => {
+            this.setState({
+              selectedTab: 'blueTab',
+            });
+          }}
+          selected={this.state.selectedTab === 'blueTab'}
+          selectedIcon={<div style={{
+            width: '22px',
+            height: '22px',
+            background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat', }}
+                        />
+          }
+          title="Life"
+        >
+          11111111
+        </TabBar.Item>
+        <TabBar.Item
+          badge={'new'}
+          data-seed="logId1"
+          icon={
+            <div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat', }}
+            />
+          }
+          key="Koubei"
+          onPress={() => {
+            this.setState({
+              selectedTab: 'redTab',
+            });
+          }}
+          selected={this.state.selectedTab === 'redTab'}
+          selectedIcon={
+            <div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat', }}
+            />
+          }
+          title="Koubei"
+        >
+          22222222222
+        </TabBar.Item>
+        <TabBar.Item
+          dot
+          icon={
+            <div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat', }}
+            />
+          }
+          key="Friend"
+          onPress={() => {
+            this.setState({
+              selectedTab: 'greenTab',
+            });
+          }}
+          selected={this.state.selectedTab === 'greenTab'}
+          selectedIcon={
+            <div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat', }}
+            />
+          }
+          title="Friend"
+        >
+          3333333333
+        </TabBar.Item>
+        <TabBar.Item
+          icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg', }}
+          key="my"
+          onPress={() => {
+            this.setState({
+              selectedTab: 'yellowTab',
+            });
+          }}
+          selected={this.state.selectedTab === 'yellowTab'}
+          selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg', }}
+          title="My"
+        >
+          44444444
+        </TabBar.Item>
+      </TabBar>
     </div>);
   }
 }
