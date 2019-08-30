490e5585b1923d53daf810e284180cfcfb85d370
kakuilan <kakuilan@163.com>
2019-08-30 19:17:47 +0800

    拆分底部栏成组件Footbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { Icon as Aicon, } from 'antd';
-import { Drawer, NavBar, TabBar, Icon, } from 'antd-mobile';
-import Sidebar from './components/Sidebar';
+import { Drawer, NavBar, Icon, } from 'antd-mobile';
 import {connect, } from 'react-redux';
+import Sidebar from './components/Sidebar';
+import Footbar from './components/Footbar';
 import {toggleSlidebar, } from './redux/actions/slidebar';
-import {switchFooterTab, } from './redux/actions/footer';
 import './styles/App.less';
 import thumbImg from './assets/image/gray.png';
 
 class App extends React.Component {
-  state = {
-    footerActive: 'home',
-    footerHidden: false,
-  }
-
   render() {
     const menus = [
       {
@@ -56,68 +50,7 @@ class App extends React.Component {
         主体内容在这里。Github battle
       </Drawer>
 
-      <TabBar
-        barTintColor="white"
-        hidden={this.state.footerHidden}
-        tintColor="#33A3F4"
-        unselectedTintColor="#949494"
-      >
-        <TabBar.Item
-          data-seed="logId"
-          icon={<Aicon style={{ fontSize: '20px', }}
-            type="home"
-                />}
-          key="Home"
-          onPress={() => this.props.switchFooterTab('home')}
-          selected={this.props.footer.footerActive === 'home'}
-          selectedIcon={<Aicon style={{ fontSize: '20px', }}
-            theme="twoTone"
-            type="home" />}
-          title="首页"
-        >
-        </TabBar.Item>
-        <TabBar.Item
-          icon={<Aicon style={{ fontSize: '20px', }}
-            type="compass"
-                />}
-          key="Battle"
-          onPress={() => this.props.switchFooterTab('battle')}
-          selected={this.props.footer.footerActive === 'battle'}
-          selectedIcon={<Aicon style={{ fontSize: '20px', }}
-            theme="twoTone"
-            type="compass"
-                        />}
-          title="对比"
-        >
-        </TabBar.Item>
-        <TabBar.Item
-          icon={<Aicon style={{ fontSize: '20px', }}
-            type="fire"
-          />}
-          key="Fire"
-          onPress={() => this.props.switchFooterTab('fire')}
-          selected={this.props.footer.footerActive === 'fire'}
-          selectedIcon={<Aicon style={{ fontSize: '20px', }}
-            theme="twoTone"
-            type="fire" />}
-          title="热门"
-        >
-        </TabBar.Item>
-        <TabBar.Item
-          icon={<Aicon style={{ fontSize: '20px', }}
-            type="question-circle"
-          />}
-          key="Search"
-          onPress={() => this.props.switchFooterTab('search')}
-          selected={this.props.footer.footerActive === 'search'}
-          selectedIcon={<Aicon style={{ fontSize: '20px', }}
-            theme="twoTone"
-            type="question-circle"
-          />}
-          title="搜索"
-        >
-        </TabBar.Item>
-      </TabBar>
+      <Footbar></Footbar>
     </div>);
   }
 }
@@ -126,7 +59,6 @@ class App extends React.Component {
 const mapStateToProps = (state) => {
   return {
     slidebar: state.slidebar,
-    footer: state.footer,
   };
 };
 
@@ -136,10 +68,6 @@ const mapDispatchToProps = (dispatch) => {
     toggleSlidebar: () => {
       console.log('toggleSlidebar 触发后进入分发器dispatch, 0000');
       dispatch(toggleSlidebar());
-    },
-    switchFooterTab: (name) => {
-      console.log('switchFooterTab 触发后进入分发器dispatch, 0000');
-      dispatch(switchFooterTab(name));
     },
   };
 };
--- src/components/Footbar.js
+++ src/components/Footbar.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { Icon as Aicon, } from 'antd';
 import { TabBar, } from 'antd-mobile';
+import {connect, } from 'react-redux';
+import {switchFooterTab, } from '../redux/actions/footer';
 
 class Footbar extends React.Component {
   constructor(props) {
@@ -10,18 +12,87 @@ class Footbar extends React.Component {
 
   render() {
     return (
-      <List>
-        {this.props.menus.map((menu, index) => {
-          return (
-            <List.Item key={index}
-              thumb={menu.thumb}
-            >
-              {menu.title}
-            </List.Item>);
-        })}
-      </List>
+      <TabBar
+        barTintColor="white"
+        hidden={this.props.footer.footerHidden}
+        tintColor="#33A3F4"
+        unselectedTintColor="#949494"
+      >
+        <TabBar.Item
+          data-seed="logId"
+          icon={<Aicon style={{ fontSize: '20px', }}
+            type="home"
+                />}
+          key="Home"
+          onPress={() => this.props.switchFooterTab('home')}
+          selected={this.props.footer.footerActive === 'home'}
+          selectedIcon={<Aicon style={{ fontSize: '20px', }}
+            theme="twoTone"
+            type="home" />}
+          title="首页"
+        >
+        </TabBar.Item>
+        <TabBar.Item
+          icon={<Aicon style={{ fontSize: '20px', }}
+            type="compass"
+                />}
+          key="Battle"
+          onPress={() => this.props.switchFooterTab('battle')}
+          selected={this.props.footer.footerActive === 'battle'}
+          selectedIcon={<Aicon style={{ fontSize: '20px', }}
+            theme="twoTone"
+            type="compass"
+                        />}
+          title="对比"
+        >
+        </TabBar.Item>
+        <TabBar.Item
+          icon={<Aicon style={{ fontSize: '20px', }}
+            type="fire"
+          />}
+          key="Fire"
+          onPress={() => this.props.switchFooterTab('fire')}
+          selected={this.props.footer.footerActive === 'fire'}
+          selectedIcon={<Aicon style={{ fontSize: '20px', }}
+            theme="twoTone"
+            type="fire" />}
+          title="热门"
+        >
+        </TabBar.Item>
+        <TabBar.Item
+          icon={<Aicon style={{ fontSize: '20px', }}
+            type="question-circle"
+          />}
+          key="Search"
+          onPress={() => this.props.switchFooterTab('search')}
+          selected={this.props.footer.footerActive === 'search'}
+          selectedIcon={<Aicon style={{ fontSize: '20px', }}
+            theme="twoTone"
+            type="question-circle"
+          />}
+          title="搜索"
+        >
+        </TabBar.Item>
+      </TabBar>
     );
   }
 }
 
-export default Footbar;
+// 将state映射到props
+const mapStateToProps = (state) => {
+  return {
+    footer: state.footer,
+  };
+};
+
+// 绑定分发器
+const mapDispatchToProps = (dispatch) => {
+  return {
+    switchFooterTab: (name) => {
+      console.log('switchFooterTab 触发后进入分发器dispatch, 0000');
+      dispatch(switchFooterTab(name));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Footbar);
