c4c8268b60678e2309296d6100662f44ec7d6be5
kakuilan <kakuilan@163.com>
2019-09-04 15:00:14 +0800

    修改battle页面

--- src/pages/Battle.js
+++ src/pages/Battle.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import { InputItem, WhiteSpace, Button, } from 'antd-mobile';
+import { Avatar, } from 'antd';
 
 class Battle extends React.Component {
-  render() {
-    return (<div>
-      <h1>对比战斗</h1>
-    </div>);
-  }
+      onChange = () => {
+
+      }
+
+      render() {
+        return (<div>
+          <h1>对比战斗</h1>
+          <InputItem
+            clear
+            onChange={this.onChange}
+            placeholder="请输入选手A"
+            value={''}
+          >
+            <Avatar className="avatar"
+              icon="user"
+              src=""
+            />
+          </InputItem>
+          <WhiteSpace />
+          <InputItem
+            clear
+            onChange={this.onChange}
+            placeholder="请输入选手B"
+            value={''}
+          >
+            <Avatar className="avatar"
+              icon="user"
+              src=""
+            />
+          </InputItem>
+          <WhiteSpace />
+          <Button inline
+            style={{ marginRight: '4px', }}
+            type="primary"
+          >开始PK</Button>
+        </div>);
+      }
 }
 
 export default Battle;
