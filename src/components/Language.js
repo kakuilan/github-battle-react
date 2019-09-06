9891fd6f6bc8debf258df1e38c459b7a986132e4
kakuilan <kakuilan@163.com>
2019-09-06 14:33:21 +0800

    Popular页面

---
+++ src/components/Language.js
@@ -0,0 +1,41 @@
+// 编程语言组件
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Language extends React.Component {
+  render() {
+    const languages = [
+      'All',
+      'Go',
+      'Javascript',
+      'Ruby',
+      'Java',
+      'Shell',
+      'Python',
+      'PHP',
+    ];
+
+    return (
+      <ul className="languages">
+        {/** 以下.bind函数绑定: bind()方法创建一个新的函数，在bind()被调用时，这个新函数的this被bind的第一个参数指定，其余的参数将作为新函数的参数供调用时使用。 */}
+        {/** 参考 https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Function/bind */}
+        {languages.map((lang) => (
+          <li
+            className={lang === this.props.selectedLang ? 'selected' : ''}
+            key={lang}
+            onClick={this.props.onSelect.bind(null, lang)}
+          >
+            {lang}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+Language.propTypes = {
+  selectedLang: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
+
+export default Language;
--- src/pages/Popular.js
+++ src/pages/Popular.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import animate from '@jam3/gsap-promise';
+import Language from '../components/Language';
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLang: 'All', // default state
+    };
+  }
+
   componentDidMount() {
     animate.from(this.topHeader, 0.2, { y: -200, delay: 0.1, });
   }
 
+  updateLanguage(lang) {
+    return lang;
+  }
+
   render() {
-    return (<div>
-      <h1 ref={(c) => { this.topHeader = c; }}>热门项目</h1>
-    </div>);
+    return (
+      <div>
+        <h1 ref={(c) => { this.topHeader = c; }}>热门项目</h1>
+        <Language
+          onSelect={this.updateLanguage}
+          selectedLang={this.state.selectedLang}
+        />
+      </div>
+    );
   }
 }
 
