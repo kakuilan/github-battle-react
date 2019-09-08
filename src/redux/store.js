0592b7343255e54136a36690f9d3f7b0626d91c8
kakuilan <kakuilan@163.com>
2019-09-08 22:04:10 +0800

    使用Redux DevTools Extension插件

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,12 +7,18 @@ import combineReducers from './reducers.js';
 
 export const history = createBrowserHistory();
 
+// 使用Redux DevTools Extension浏览器调试插件
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+  // 有指定扩展选项，像name, actionsBlacklist, actionsCreators, serialize...
+  }) : compose;
+
 // preloadedState为初始状态
 export default function configureStore(preloadedState) {
   const store = createStore(
     combineReducers(history), // root reducer with router state
     preloadedState,
-    compose(
+    composeEnhancers(
       applyMiddleware(
         routerMiddleware(history), // for dispatching history actions
         ReduxThunk
